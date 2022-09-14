cd9755ef1a3214e07c33b46aa8a737ecb6b83194
Sagittarius-22 <112104249+Sagittarius-22@users.noreply.github.com>
2022-09-14 17:54:56 +0800

    脚手架中main.js的问题
    脚手架中main.js的问题

---
+++ 2022.09.14-Vue cli_main.js/src1/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <School></School>
+  </div>
+</template>
+
+<script>
+import School from './components/School.vue'
+export default {
+    name:'App',
+    components:{
+        School
+    },
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
